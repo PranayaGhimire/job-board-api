cf2fab211886a57fd935e75425effc66d830d2db
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-09-07 16:44:57 +0545

    Added Role Based Access Control

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { applyJob, getCompanyApplications, getUserApplications } from "../controllers/applicationController.js";
+import { authorizeRoles } from "../middleware/roleMiddleware.js";
 const router = express.Router();
-router.post("/",protect,applyJob);
-router.get("/user",protect,getUserApplications);
-router.get("/company",protect,getCompanyApplications);
+router.post("/",protect,authorizeRoles("user"),applyJob);
+router.get("/user",protect,authorizeRoles("user"),getUserApplications);
+router.get("/company",protect,authorizeRoles("company"),getCompanyApplications);
 export default router;
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { createJob, getCompanyJobs, getJobs } from "../controllers/jobController.js";
+import { authorizeRoles } from "../middleware/roleMiddleware.js";
 const router = express.Router();
-router.post("/",protect,createJob);
+router.post("/",protect,authorizeRoles('company'),createJob);
 router.get("/",getJobs);
-router.get("/company",protect,getCompanyJobs);
+router.get("/company",protect,authorizeRoles('company'),getCompanyJobs);
 export default router;
 
